583fc0f9660c6413d5378eb57116d05f629e900c
Dovydas <Cernauskas>
2024-02-01 08:53:01 +0200

    Pakeitimai v2

--- src/components/productListCard/ProductListCard.jsx
+++ src/components/productListCard/ProductListCard.jsx
@@ -13,7 +13,7 @@ return (
                         </div>
                     </div>
                     <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                        <div className="text-center"><Link className="btn btn-primary mt-auto" to={`/product/${props.productData.id}`}>Apžvelgti</Link></div>
+                        <div className="text-center"><Link className="btn btn-dark mt-auto w-100" to={`/product/${props.productData.id}`}>Apžvelgti</Link></div>
                     </div>
                 </div>
             </div>
